568493c7a0a45775beb336e31527211bca43ad76
Jon Granered <jon.granefred@gmail.com>
2014-12-02 13:47:23 +0100

    table.css finns med en enkelt table struktur se http://purecss.io/tables/ http://purecss.io/tables/ http://purecss.io/tables/ http://purecss.io/tables/

--- js/main.js
+++ js/main.js
@@ -1,25 +1,52 @@
 //DOM Ready :)
 $(function() {
 
-  $.ajax({
+  //find stock for a single isbn
+  function currentStockOfIsbn(formInfo) {
+    $.ajax({
+      // Use Nodebite's magic library
       url:"libs/sql-ajax-json.php",
+      // Expect json in return
       dataType: "json",
       data: {
+        // Read SQL questions from this file
         sql: "sql/product-questions.sql",
-        run: "select book by isbn",
-        isbn: $('#isbn').val()
+        // Run the query named all products
+        run: "current price on books",
+        isbn: formInfo.isbn
       },
-      success:output
+      success: function(data) {
+
+      },
+      error: function(data) {
+
+      }
+    });
+  }
+
+  $(".submitForm").submit(function() {
+    var formInfo = {};
+    $(this).find("input").not("input[type='submit']").each(function() {
+      formInfo[this.name] = $(this).val();
     });
 
+    console.log("formInfo: ", formInfo);
+
+    //check if formInfo.isbn actually contains any characters 
+    //(if the user is actually searching for something specific)
+    if (formInfo.isbn.length) {
+      //search for that specific thing
+      currentStockOfIsbn(formInfo);
+    } else {
+      //else just find everything
+      currentStockOfAll();
+    }
+
+    //always return false in a jQuery .submit() to prevent page reload
+    return false;
+  });
 
 });
-
-
-
-
-
-
 
 function myFunction() {
     document.getElementById(id="input1")
